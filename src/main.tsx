3d74ebdcd24ae08235462298fc9c9160b81b838f
MaksPronevich <maks.pronevich@gmail.com>
2024-03-23 06:43:45 +0300

    #02-fix-errors-and-crop-video

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import './index.css'
-import React from 'react'
-import { App } from "./App.tsx";
-import ReactDOM from 'react-dom/client'
+import "./index.css";
+import React from "react";
+import { App } from "./App";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
